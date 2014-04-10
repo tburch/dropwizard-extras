1c63f01ffe00143ab57dd1ffc8e78097c5c00de4
Tristan Burch <me@tristanburch.com>
2014-04-10 10:50:57 -0600

    typo

--- dropwizard-extras-heroku/src/main/java/com/lowtuna/dropwizard/extras/heroku/RequestIdFilter.java
+++ dropwizard-extras-heroku/src/main/java/com/lowtuna/dropwizard/extras/heroku/RequestIdFilter.java
@@ -6,13 +6,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 
 public class RequestIdFilter implements ContainerRequestFilter {
-    private static final String HEOKU_REQUEST_ID_HEADER = "X-Request-ID";
+    private static final String HEROKU_REQUEST_ID_HEADER = "X-Request-ID";
     private static final String REQUEST_ID_LOG_NAME = "requestId";
 
     private final String headerName;
 
     public RequestIdFilter() {
-        this(HEOKU_REQUEST_ID_HEADER);
+        this(HEROKU_REQUEST_ID_HEADER);
     }
 
     public RequestIdFilter(String headerName) {
